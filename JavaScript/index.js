3e6737cee791cb35de4f55c2edae1ca3334b829f
Valeriia2707 <valeriakoval27072007@gmail.com>
2025-05-16 13:45:21 +0200

    added functionl to the 'next' button

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,13 +1,21 @@
 let questionCounter = 1;
 let number = 0;
+let groupCounter = 1;
+let userAnswer = {}
 function buttonsClick (button, type){
-    if (questionCounter == 6){
+    
+    if (questionCounter == 26){
         return;
     }
+    
+    let selectedValue = button.dataset.value;
+    userAnswer[questionCounter] = selectedValue;
+    console.log(userAnswer);
+
     let percentElement = document.querySelector(".progress__percentage");
     let percentBarFill = document.querySelector(".progress__fill")
-    let currentAnswerBox = document.getElementById(`${questionCounter}`);
-    let nextAnswerBox = document.getElementById(`${questionCounter + 1}`);
+    let currentAnswerBox = document.getElementById(`question-${questionCounter}`);
+    let nextAnswerBox = document.getElementById(`question-${questionCounter + 1}`);
     
     if (type == 'green'){
         button.style.backgroundColor = " #21978b";
@@ -24,8 +32,27 @@ function buttonsClick (button, type){
     }
     questionCounter++;
 
-    number += 20;
+    number += 4;
     percentElement.innerHTML = `${number + "%"}`;
 
     percentBarFill.style.width = `${number + "%"}`;
+
+}
+
+function nextButton() {
+    if (![6, 11, 16, 21, 26].includes(questionCounter)){
+        return;
+    }
+    let currentAnswerGroup = document.getElementById(`group-${groupCounter}`);
+    let nextAnswerGroup = document.getElementById(`group-${groupCounter + 1}`);
+    currentAnswerGroup.classList.remove("quiz-page__group--active");
+    if (nextAnswerGroup){
+        nextAnswerGroup.classList.add("quiz-page__group--active");
+    }
+    groupCounter++;
+    window.scrollTo({
+        top: 400,
+        behavior: "smooth"
+    });
+    
 }
